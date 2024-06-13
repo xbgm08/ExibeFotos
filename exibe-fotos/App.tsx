f1394723ad9bb15dab20b83649fc854765e67de4
xbgm08 <gabrielbraulino@gmail.com>
2024-06-13 16:24:00 -0300

    feat(p2-pdm): adiciona e estiliza o componente lista

--- exibe-fotos/App.tsx
+++ exibe-fotos/App.tsx
@@ -1,24 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { 
+import {
   Pressable,
-  StyleSheet, 
-  Text, 
-  View 
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Image
 } from 'react-native';
+import { useState } from 'react';
 
-export default function App() { 
+interface Imagem {
+  id?: string;
+  url: string;
+}
+
+export default function App() {
+  const [imagens, setImagens] = useState<Imagem[]>([])
+
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}>Aplicativo de Exibição de Fotos de Gatos</Text><br/>
+      <Text style={styles.titulo}>Aplicativo de Exibição de Fotos de Gatos</Text><br />
       <Pressable
         style={styles.button}
-        // onPress={função que vai acionar o consumo da api}  
-        >
+      // onPress={função que vai acionar o consumo da api}  
+      >
         <Text
-          style={styles.buttonText}>          
+          style={styles.buttonText}>
           Exibir fotos
         </Text>
       </Pressable>
+
+      {imagens.length > 0 ?? 
+        <FlatList
+          style={styles.list}
+          keyExtractor={item => item.id!}
+          data={imagens}
+          renderItem={imagem => (
+            <View
+              style={styles.listItem}>
+              <Image
+                style={styles.listItemImage}
+                source={{
+                  uri: imagem.item.url,
+                }}
+              />              
+            </View>
+          )}
+        />
+      }
     </View>
   );
 }
@@ -28,21 +57,40 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-  },  
+  },
   titulo: {
     color: 'red',
     alignItems: 'center',
     fontFamily: 'fantasy',
   },
-  button:{ 
+  button: {
     width: '80%',
-    backgroundColor: '#0096F3', 
+    backgroundColor: '#0096F3',
     padding: 8,
     borderRadius: 4,
     marginBottom: 4,
   },
-  buttonText:{
+  buttonText: {
     color: 'white',
     textAlign: 'center'
-  },  
+  },
+  list: {
+    borderWidth: 1,
+    borderColor: 'lightgray',
+    width: '80%',
+    borderRadius: 4,
+    marginBottom: 8
+  },
+  listItem: {
+    padding: 12,
+    borderBottomWidth: 1,
+    borderBlockColor: 'gray',
+    borderRadius: 8,
+    backgroundColor: '#F0F0F0',
+    margin: 8,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  listItemImage: {
+  },
 });
